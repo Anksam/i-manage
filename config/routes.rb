2db3392f121ac152457b08a77d5269e7b5b88e59
Ankesh Samarth <aankeshsamarth@gmail.com>
2017-06-14 01:42:18 +0530

    Added static_pages controller, root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "static_pages#landing_page"
+  
+  # static_pages
+  # controller :static_pages do
+  #   get :landing_page
+  # end
+
 end
