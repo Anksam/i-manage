0d7878ad26b2b2306b7079868959d5541afa5769
Ankesh Samarth <aankeshsamarth@gmail.com>
2017-06-16 16:38:29 +0530

    Included Validations in Income and Expense, migrated Expense

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,5 @@
 class Expense < ApplicationRecord
   belongs_to :user
+  validates :amount, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616094719) do
+ActiveRecord::Schema.define(version: 20170616104204) do
+
+  create_table "expenses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "amount"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id", using: :btree
+  end
 
   create_table "incomes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "amount"
@@ -43,5 +51,6 @@ ActiveRecord::Schema.define(version: 20170616094719) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "expenses", "users"
   add_foreign_key "incomes", "users"
 end
