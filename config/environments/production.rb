cbcdb7256638fea58014bd3ddf10c39b9d351501
Ankesh Samarth <aankeshsamarth@gmail.com>
2017-06-19 02:24:29 +0530

    Config changes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -82,10 +82,8 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
   # Devise
   config.action_mailer.perform_deliveries = true
-  # default url option
-  config.action_mailer.default_url_options = { host: 'i-manage-app.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'i-manage-app.herokuapp.com', :protocol => 'http' }
   config.action_mailer.delivery_method = :smtp
 end
