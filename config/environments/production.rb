731cc63ab6ece68906a1edee35685d8ee2e1a741
Ankesh Samarth <aankeshsamarth@gmail.com>
2017-06-19 02:31:19 +0530

    Changes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,12 +54,12 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "i-manage_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "storeapp_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -82,8 +82,8 @@ Rails.application.configure do
   end
 
   # Do not dump schema after migrations.
-  # Devise
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'i-manage-app.herokuapp.com', :protocol => 'http' }
+  config.active_record.dump_schema_after_migration = false
+  # default url options
+  config.action_mailer.default_url_options = { host: 'i-manage-app.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 end
